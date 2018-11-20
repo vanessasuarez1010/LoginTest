c292e2833f0869fbc29bc72e8c94d5f50debe44e
sindy <sindyrengifosuarez@hotmail.com>
2018-11-20 15:46:55 -0500

    nuevas cosas

--- Test Suites/Login.ts
+++ Test Suites/Login.ts
@@ -4,7 +4,7 @@
    <name>Login</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-11-19T16:13:37</lastRun>
+   <lastRun>2018-11-20T15:26:45</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
